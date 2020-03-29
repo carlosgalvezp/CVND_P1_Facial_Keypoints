271de649b187b019d1573154dbd54631582e1b3a
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-03-29 11:55:05 +0200

    Fix install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -3,4 +3,5 @@ set -euxo pipefail
 
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
+pip3 install wheel==0.34.2  # Required to install jupyter later on
+pip3 install -r requirements.txt
